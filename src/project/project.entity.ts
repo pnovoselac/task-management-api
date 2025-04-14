b56313fce7912de6937f03aa7863fb02f3d24db3
pnovoselac <paulina.novoselac@gmail.com>
2025-04-14 16:29:22 +0200

    Add change based on review

--- src/project/project.entity.ts
+++ src/project/project.entity.ts
@@ -12,7 +12,6 @@ import {
 import { Task } from "../task/task.entity";
 import { ProjectRepository } from "./project.repository";
 import { User } from "../user/user.entity";
-import { ApiProperty } from "@nestjs/swagger";
 
 export enum Visibility {
   PUBLIC = "public",
@@ -22,21 +21,12 @@ export enum Visibility {
 @Entity({ repository: () => ProjectRepository })
 export class Project {
   [OptionalProps]?: "createdAt" | "updatedAt" | "deletedAt";
-  @ApiProperty({
-    description: "Project identificator",
-    example: 1,
-  })
   @PrimaryKey()
   id!: number;
 
-  @ApiProperty({ description: "Project name", example: "Task Management API" })
   @Property()
   title!: string;
 
-  @ApiProperty({
-    description: "Project description",
-    example: "REST API for a task management application",
-  })
   @Property()
   description!: string;
 
@@ -49,27 +39,15 @@ export class Project {
   @Property({ nullable: true })
   deletedAt!: Date | null;
 
-  @ApiProperty({
-    description: "Tasks assigned for project",
-    type: [Task],
-  })
   @OneToMany(() => Task, (task) => task.project)
   tasks = new Collection<Task>(this);
 
-  @ApiProperty({ description: "Project owner", type: () => User })
   @ManyToOne(() => User)
   owner!: User;
 
-  @ApiProperty({ description: "Project members", type: [User] })
   @ManyToMany(() => User, (user) => user.memberProjects, { owner: true })
   members = new Collection<User>(this);
 
-  @ApiProperty({
-    description: "Project visibility",
-    enum: Visibility,
-    example: Visibility.PRIVATE,
-    default: Visibility.PRIVATE,
-  })
   @Enum(() => Visibility)
   visibility: Visibility = Visibility.PRIVATE;
 }
