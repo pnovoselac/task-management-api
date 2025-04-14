df0f25ec16869560f5b905f170814817347a26be
pnovoselac <paulina.novoselac@gmail.com>
2025-04-14 15:48:03 +0200

    Add change based on review

--- src/file/file.entity.ts
+++ src/file/file.entity.ts
@@ -13,27 +13,15 @@ import { ApiProperty } from "@nestjs/swagger";
 export class File {
   [OptionalProps]?: "createdAt";
 
-  @ApiProperty({
-    description: "Unique identifier for the file",
-    example: 1,
-  })
   @PrimaryKey()
   id!: number;
 
-  @ApiProperty({
-    description: "URL of the file",
-    example: "https://storage.googleapis.com/task-files/example.pdf",
-  })
   @Property()
   url!: string;
 
   @Property({ onCreate: () => new Date() })
   createdAt: Date = new Date();
 
-  @ApiProperty({
-    description: "Task to which the file belongs",
-    type: () => Task,
-  })
   @ManyToOne(() => Task)
   task!: Task;
 }
